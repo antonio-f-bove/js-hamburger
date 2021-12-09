34085598fa45bbb2bbe5438ca642adf6385ab851
mmUggio <antoniofbove@gmail.com>
2021-12-09 17:43:01 +0100

    toggle ok

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const toggleMenu = document.querySelectorAll('.hamburger-menu');
-console.log(toggleMenu);
-const toggleButton = document.querySelectorAll('.header-right > a');
-console.log(toggleButton);
+const toggleMenu = document.querySelector('.hamburger-menu');
+const toggleButton = document.querySelector('.header-right > a');
+const closeButton = document.querySelector('.close');
 
 toggleButton.addEventListener('click', function () {
   toggleMenu.classList.toggle('active');
-  console.log(toggleMenu.classlist);
+})
+closeButton.addEventListener('click', function () {
+  toggleMenu.classList.toggle('active');
 })
